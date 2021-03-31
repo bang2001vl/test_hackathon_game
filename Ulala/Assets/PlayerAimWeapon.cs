ec62486be1d9627a5e2361e933b79170257bd23c
UIT.19520266 <19520266@gm.uit.edu.vn>
2021-04-01 00:49:55 +0700

    Thach1

---
+++ Ulala/Assets/PlayerAimWeapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+
+public class PlayerAimWeapon : MonoBehaviour
+{
+    Transform aimTransform;
+    private void Awake()
+    {
+        aimTransform = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aimTransform == null) return;
+        Vector3 mouseposition = Input.mousePosition;
+        Vector3 aimdirection = (mouseposition - transform.position).normalized;
+        var angle = Mathf.Atan2(aimdirection.y, aimdirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+        Debug.Log(angle);
+    }
+}
