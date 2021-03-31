98caf70b41776ed5a0fcdb95e1af1527e01590e9
UIT.19520266 <19520266@gm.uit.edu.vn>
2021-04-01 03:53:26 +0700

    Done

---
+++ Ulala/Assets/BulletController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BulletType
+{
+    Enemy, Player
+}
+public class BulletController : MonoBehaviour
+{
+    public static int Damage = 5;
+    public int Speed;
+    private Vector3 _direction;
+    private BulletType _type;
+    public BulletController() { }
+    public void Init(Vector3 direction, Vector3 position, BulletType type)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, 0, angle);
+
+        _direction = direction;
+        _type = type;
+        transform.position = position;
+    }
+    private void Update()
+    {
+        transform.position += _direction.normalized * Speed * Time.deltaTime;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<EnemyController>().Damage();
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Ulala/Assets/PlayerAimWeapon.cs
+++ Ulala/Assets/PlayerAimWeapon.cs
@@ -6,19 +6,49 @@ using UnityEngine.Diagnostics;
 public class PlayerAimWeapon : MonoBehaviour
 {
     Transform aimTransform;
+    public GameObject Bullet;
+    public int Health;
+    private void Start()
+    {
+        
+    }
     private void Awake()
     {
-        aimTransform = gameObject.transform;
+        aimTransform = transform.Find("Aim");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (aimTransform == null) return;
-        Vector3 mouseposition = Input.mousePosition;
-        Vector3 aimdirection = (mouseposition - transform.position).normalized;
-        var angle = Mathf.Atan2(aimdirection.y, aimdirection.x) * Mathf.Rad2Deg;
+        AimMouse();
+        ClickEvent();
+    }
+    private void AimMouse()
+    {
+
+        Vector3 aimdirection = AimDirection();
+        float angle = Mathf.Atan2(aimdirection.y, aimdirection.x) * Mathf.Rad2Deg;
         aimTransform.eulerAngles = new Vector3(0, 0, angle);
-        Debug.Log(angle);
+    }
+    private void ClickEvent()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.Shot();
+        }
+    }
+
+    private void Shot()
+    {
+        GameObject bullet = Instantiate(Bullet);
+        bullet.GetComponent<BulletController>().Init(AimDirection(),transform.position,BulletType.Player);
+        Destroy(bullet, 3f);
+    }
+
+    private Vector3 AimDirection()
+    {
+        Vector3 mouseposition = Input.mousePosition;
+        Vector3 screenpoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        return mouseposition - screenpoint;
     }
 }
