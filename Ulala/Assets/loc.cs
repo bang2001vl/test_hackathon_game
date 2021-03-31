6a69cc9bb60ec13e5664d67e4ac911a53517ce6a
UIT.19521973 <67250352+Nhuc-Huynh@users.noreply.github.com>
2021-04-01 02:44:19 +0700

    Update_BG_level1

---
+++ Ulala/Assets/loc.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    float speed = 5f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += transform.right * speed * Time.deltaTime;
+    }
+
+   
+    // Update is called once per frame
+  
+}
